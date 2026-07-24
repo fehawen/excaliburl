78ccb3f969c95b2544ca37f12cf309f7f66f91c6
fehawen <fehawen@pm.me>
2026-07-24 17:45:20 +0200

    Try to clean up html

--- main.go
+++ main.go
@@ -325,6 +325,12 @@ func (p *AbsoluteProcessor) Process(data []byte, file string, emit Emitter) {
 	}
 }
 
+func cleanHTML(s string) string {
+	s = strings.TrimRight(s, ">")
+	s = strings.TrimRight(s, "/")
+	return s
+}
+
 type HTMLProcessor struct {
 	re *regexp.Regexp
 }
@@ -352,7 +358,7 @@ func (p *HTMLProcessor) Process(data []byte, file string, emit Emitter) {
 		emit(Result{
 			File:      file,
 			Processor: p.Name(),
-			Raw:       string(m[2]),
+			Raw:       cleanHTML(string(m[2])),
 		})
 	}
 }
